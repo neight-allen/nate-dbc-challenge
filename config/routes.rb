9fce2b0275dddf38cbda5e078f19d5d50c1fbdb5
Nate Allen <neight.allen@gmail.com>
2015-10-31 13:03:46 -0500

    Scaffolded album

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :albums
   resources :artists
   root to: "artists#index"
   devise_for :users#, :controllers => { :registrations => 'devise/registrations', :sessions => 'sessions', :passwords => 'passwords'}
