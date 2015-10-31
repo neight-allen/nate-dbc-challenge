e241932f925e99cfefe018aa7ecf9ca886223ddc
Nate Allen <neight.allen@gmail.com>
2015-10-31 14:04:12 -0500

    Scaffolded songs and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031180334) do
+ActiveRecord::Schema.define(version: 20151031190321) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20151031180334) do
   end
 
   add_index "artists", ["user_id"], name: "index_artists_on_user_id"
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "songs", ["album_id"], name: "index_songs_on_album_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
